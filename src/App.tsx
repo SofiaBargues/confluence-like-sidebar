d1f6990013598156e61165f31c207cd59a218bcd
Sofia Bargues <barguessofia@gmail.com>
2024-08-11 16:33:00 +0100

    IS WORKING

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
 import DATA from "./data.tsx";
-import Menu from "./Menu.tsx";
+import MenuItemComponent from "./Menu.tsx";
 
 function Sidebar() {
-  // const [count, setCount] = useState(0);
-  // console.log(DATA[0].children[0].label);
   return (
-    <div>
-      <div>{DATA[0].label}</div>
-      {/* <div>{DATA[0].children[0].label}</div>
-      <div>
-        <div>{DATA[0].children[0].children[0].label}</div>
-        <div>{DATA[0].children[0].children[1].label}</div>
-        <div>{DATA[0].children[0].children[2].label}</div>
-      </div> */}
-      <Menu items={DATA[0]} />
-    </div>
+    <button>
+      <MenuItemComponent item={DATA[0]} />
+    </button>
   );
 }
 
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,6 +1,26 @@
 import { MenuItem } from "./data";
+import { useState } from "react";
 
-export default function Menu({ items }: { items: MenuItem }) {
-  const listItems = items.children?.map((item) => <li>{item.label}</li>);
-  return <ul>{listItems}</ul>;
+export default function MenuItemComponent({ item }: { item: MenuItem }) {
+  const [extend, setExtend] = useState(false);
+
+  function handleClick() {
+    setExtend(!extend);
+  }
+
+  const listItems = item.children?.map((childItem) => (
+    <li>
+      {" "}
+      <MenuItemComponent item={childItem} />
+    </li>
+  ));
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        <p>{item.label}</p>
+      </button>
+      <ul>{extend ? listItems : " "}</ul>
+    </>
+  );
 }
